9c49b97ad77120ae3c0cf187652b69164491ae83
Diegokh <diegoblanque04@outlook.com>
2025-03-03 16:28:07 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/actividadgit/ActividadGit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividadgit;
+
+/**
+ *
+ * @author kali
+ */
+public class ActividadGit {
+
+    public static void main(String[] args) {
+        System.out.println("ACTIVIDAD 27--Uso de GIT en Netbeans!");
+        System.out.println("Realzio el primer commit");
+        
+    }
+}
